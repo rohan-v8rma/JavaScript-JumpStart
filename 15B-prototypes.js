81942e38644f530545dfcd0b28040ffd8cca0cb2
Rohan Verma <rv725555@gmail.com>
2022-05-16 20:49:03 +0530

    improv: Diff. b/w '__proto__' and 'prototype'

--- 15B-prototypes.js
+++ 15B-prototypes.js
@@ -1,6 +1,6 @@
 /*
             INDEX
-1. About prototypes & the prototype chain
+1. About prototypes, the prototype chain and the two types of prototypes
 2. Internal properties of an object
 3. Creating an object with a predefined prototype
 
@@ -13,20 +13,29 @@ The prototype is itself an object, so the prototype will have its own prototype,
 making what's called a prototype chain. The chain ends when we reach a prototype 
 that has null for its own prototype.
 
+? Note that there are two types of prototypes associated with an object. 
+  
+  One which the object has inherited from some other class, which can be accessed using:
+* <object-name>.__proto__ 
+  Another which is passed on to instances of that object, which can be accessed using:
+* <object-name>.prototype
+
+So, for example if an object 'hello' is an instance of the global Object class, 
+"hello.__proto__ === Object.prototype" will return 'true', since the prototype inherited
+by 'hello' is the same prototype which 'Object' passes on to instances of itself.
+
+* "Object.__proto__" will return 'undefined' since the global Object class doesn't inherit
+* from any other object.
+
 ! Nearly all objects in JavaScript are instances of the global Object class, which sits
 ! just below null on the top of the prototype chain.
 
 
 * 2. Internal properties of an object
 
-! NOTE : On most online sources, for the sake of simplicity it is written Object.prototype, however, the 
-! 'prototype', which is an "internal property" of an object, (indicated by the fact that it is 
+! NOTE : '[[prototype]]', which is an "internal property" of an object, (indicated by the fact that it is 
 ! enclosed in double square brackets "[[ ]]") isn't an identifier which can be used in 
-! actual code -- internal properties aren't accessible as members of the objects that contain them.
-
-? However, they may be made accessible by particular functions or properties, 
-? For example, the '__proto__' property is used to access the prototype of an object as a general convention
-? in most browsers.
+! actual code.
 
 * The use of this double brackets notations over single brackets is probably to avoid any possible 
 * confusion with actual bracket notation(i.e., property access).
@@ -47,8 +56,10 @@ let Prototype = {
     },
 };
 /* 
-* 'Prototype' itself is an object, so the prototype of 'Prototype' is Object.__proto__, 
-! the same as global Object class
+'Prototype' itself is an object, so the prototype of 'Prototype' is Object.prototype
+which it inherits from global Object class. 
+! NOTE : "Prototype.__proto__ === Object.prototype" will return True
+
 */
 
 let newObj = Object.create(Prototype);

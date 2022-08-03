6eaead5aefa84d0da5df76cc3f846cf62e623822
Rohan Verma <rv725555@gmail.com>
2022-08-03 21:27:14 +0530

    feat: Printing the URLs that contain a certain sub-string. - Converted anchor tag represented as an object in the DOM into a string - Notice how only one URL is logged depending on the presence of a URL - WEB CRAWLER CONCEPT

--- understanding-DOM/script.js
+++ understanding-DOM/script.js
@@ -1,6 +1,17 @@
-let htmlCollection = document.getElementsByTagName("*");
-let arr = Array.from(htmlCollection);
+let htmlCollection = window.document.links;
+let array = Array.from(htmlCollection);
 
-arr.forEach(function (element) {
-  window.console.log(element);
-})
+array.forEach(function(element) {
+    // This console logs the a (`anchor`) tag represented as an object in the DOM.
+    console.log(element);
+
+    //The element is converted to a string i.e., its url is stored in the variable `url` as a string.
+    let url = element.toString();
+
+    // This is the substring present in one of the hyperlinks
+    let sub = "name";
+
+    // We use the includes() method of strings to find whether the substring is present in the url
+    if (url.includes(sub)) {
+        console.log(url);
+    }})

4cacbaa782193ef53e81c50010d930f8ea6bb0ec
Rohan Verma <rv725555@gmail.com>
2022-09-05 00:07:01 +0530

    feat: Experimenting with event listeners for mouse events.

--- understanding-DOM/script.js
+++ understanding-DOM/script.js
@@ -1,17 +1,8 @@
-let htmlCollection = window.document.links;
-let array = Array.from(htmlCollection);
 
-array.forEach(function(element) {
-    // This console logs the a (`anchor`) tag represented as an object in the DOM.
-    console.log(element);
+const text = document.getElementById('fullheight');
 
-    //The element is converted to a string i.e., its url is stored in the variable `url` as a string.
-    let url = element.toString();
+text.addEventListener("wheel", function(event) {
 
-    // This is the substring present in one of the hyperlinks
-    let sub = "name";
-
-    // We use the includes() method of strings to find whether the substring is present in the url
-    if (url.includes(sub)) {
-        console.log(url);
-    }})
+    text.style.backgroundColor = "red";
+    console.log(event);
+} );

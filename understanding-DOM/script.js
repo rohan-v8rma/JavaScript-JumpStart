e58c292ee471433193ee78cb38caa29e51bdc7cd
Rohan Verma <rv725555@gmail.com>
2022-09-05 22:16:25 +0530

    feat: Example for understanding dates in javascript.

--- understanding-DOM/script.js
+++ understanding-DOM/script.js
@@ -1,8 +1,12 @@
 
-const text = document.getElementById('fullheight');
+let currentTime = new Date(); // An empty constructor of the Date object
 
-text.addEventListener("wheel", function(event) {
 
-    text.style.backgroundColor = "red";
-    console.log(event);
-} );
+console.log(currentTime.getFullYear()); // Logging the year from the newly created Date object
+
+let originalDate = new Date(2000, 1, 1); // Date from which we need to calculate difference.
+
+let timeDomObject = document.getElementById("time"); // DOM object whose text we need to change.
+
+timeDomObject.innerText = currentTime.getFullYear() - originalDate.getFullYear(); // Changing the text
+
